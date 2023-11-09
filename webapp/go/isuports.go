7fe3f29a379fcdb1ec769d77d049b5f08fbd4875
6rawn <103933406+6rawn@users.noreply.github.com>
2023-11-10 00:50:22 +0900

    Add retry

--- webapp/go/isuports.go
+++ webapp/go/isuports.go
@@ -1066,6 +1066,7 @@ func competitionScoreHandler(c echo.Context) error {
 		})
 	}
 
+	retry := 0
 	tx, err := tenantDB.Beginx()
 	if err != nil {
 		return fmt.Errorf("error tenantDB.Beginx: %w", err)
@@ -1080,16 +1081,25 @@ func competitionScoreHandler(c echo.Context) error {
 		return fmt.Errorf("error Delete player_score: tenantID=%d, competitionID=%s, %w", v.tenantID, competitionID, err)
 	}
 	for _, ps := range playerScoreRows {
-		if _, err := tx.NamedExecContext(
-			ctx,
-			"INSERT INTO player_score (id, tenant_id, player_id, competition_id, score, row_num, created_at, updated_at) VALUES (:id, :tenant_id, :player_id, :competition_id, :score, :row_num, :created_at, :updated_at)",
-			ps,
-		); err != nil {
-			return fmt.Errorf(
-				"error Insert player_score: id=%s, tenant_id=%d, playerID=%s, competitionID=%s, score=%d, rowNum=%d, createdAt=%d, updatedAt=%d, %w",
-				ps.ID, ps.TenantID, ps.PlayerID, ps.CompetitionID, ps.Score, ps.RowNum, ps.CreatedAt, ps.UpdatedAt, err,
-			)
+		retry = 0
+		for {
+			if _, err := tx.NamedExecContext(
+				ctx,
+				"INSERT INTO player_score (id, tenant_id, player_id, competition_id, score, row_num, created_at, updated_at) VALUES (:id, :tenant_id, :player_id, :competition_id, :score, :row_num, :created_at, :updated_at)",
+				ps,
+			); err != nil {
+				retry++
 
+				if retry == 5 {
+					return fmt.Errorf(
+						"error Insert player_score: id=%s, tenant_id=%d, playerID=%s, competitionID=%s, score=%d, rowNum=%d, createdAt=%d, updatedAt=%d, %w",
+						ps.ID, ps.TenantID, ps.PlayerID, ps.CompetitionID, ps.Score, ps.RowNum, ps.CreatedAt, ps.UpdatedAt, err,
+					)
+				}
+			}
+			if err == nil {
+				break
+			}
 		}
 	}
 	tx.Commit()
