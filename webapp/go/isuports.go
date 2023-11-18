7f4db8a919c320f9419e8a5f4aa6d104dbb2d908
zr <komatsuryo989@gmail.com>
2023-11-18 18:34:17 +0900

    fix: where clause

--- webapp/go/isuports.go
+++ webapp/go/isuports.go
@@ -636,7 +636,7 @@ func billingReportByCompetition(ctx context.Context, tenantDB dbOrTx, tenantID i
 	if err := adminDB.SelectContext(
 		ctx,
 		&vhs,
-		"SELECT player_id, created_at AS min_created_at FROM visit_history competition_id = ? GROUP BY player_id",
+		"SELECT player_id, created_at AS min_created_at FROM visit_history WHERE competition_id = ? GROUP BY player_id",
 		comp.ID,
 	); err != nil && err != sql.ErrNoRows {
 		return nil, fmt.Errorf("error Select visit_history: tenantID=%d, competitionID=%s, %w", tenantID, comp.ID, err)
