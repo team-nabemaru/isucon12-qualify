d765ca5b00f3d3a6777ed25dfcd4519fee868356
6rawn <103933406+6rawn@users.noreply.github.com>
2023-11-18 18:21:23 +0900

    Skip `insert` if row exists

--- webapp/go/isuports.go
+++ webapp/go/isuports.go
@@ -1425,10 +1425,13 @@ func competitionRankingHandler(c echo.Context) error {
 		"INSERT INTO visit_history (player_id, tenant_id, competition_id, created_at) VALUES (?, ?, ?, ?)",
 		v.playerID, tenant.ID, competitionID, now,
 	); err != nil {
-		return fmt.Errorf(
-			"error Insert visit_history: playerID=%s, tenantID=%d, competitionID=%s, createdAt=%d, updatedAt=%d, %w",
-			v.playerID, tenant.ID, competitionID, now, now, err,
-		)
+		var mysqlErr *mysql.MySQLError
+		if !(errors.As(err, &mysqlErr) && mysqlErr.Number == 1062) {
+			return fmt.Errorf(
+				"error Insert visit_history: playerID=%s, tenantID=%d, competitionID=%s, createdAt=%d, updatedAt=%d, %w",
+				v.playerID, tenant.ID, competitionID, now, now, err,
+			)
+		}
 	}
 
 	var rankAfter int64
